5f4142d1aab5a9bb30c4e597d908a3d7c9b8623c
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2023-06-11 15:40:32 -0300

    Primeira versão do projeto: SITE-LIVRARIA

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    $(".carrossel").slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+
+
+})
